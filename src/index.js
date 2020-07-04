b614179a88625b79b047150e3f955394aec67a8a
manoj puthalapattu <58524654+manojputhalapattu@users.noreply.github.com>
2020-07-04 11:10:04 +0530

    First-code

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+import React from react;
+import React-Dom from react-dom;
+
+React.render(
+  <div>
+<h1>  Crack the unacademy</h1>
+    </div>,
+getElementById("root");
+)
