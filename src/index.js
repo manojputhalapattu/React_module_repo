5b2984bdf90bafac2e55394e0caaf39d0717caf2
manoj puthalapattu <58524654+manojputhalapattu@users.noreply.github.com>
2020-07-04 11:18:55 +0530

    "-react-render"

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
-import React from react;
-import React-Dom from react-dom;
+import React from "react";
+import ReactDom from "react-dom";
 
-React.render(
+ReactDom.render(
   <div>
-<h1>  Crack the unacademy</h1>
-    </div>,
-getElementById("root");
-)
+    <h1> Crack the unacademy</h1>
+    <ul>
+      <li> i love pongal </li>
+      <li> i love chutny </li>
+      <li> i want vada </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
